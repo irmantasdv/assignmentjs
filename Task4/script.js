a8c5cb1a96f45f72f846578c1010733b75884620
bhava <irmantas.dv@gmail.com>
2022-08-18 08:25:23 +0300

    third assignment. Rendered car brand array and brand models array inside array. Added sort by models array length.

--- Task4/script.js
+++ Task4/script.js
@@ -10,3 +10,43 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+const carBrandCard = (data) => {
+    const sortedData = data.sort((a, b) => b.models.length - a.models.length);
+    console.log(sortedData);
+    sortedData.forEach(car => {
+        const cardDiv = document.createElement('div');
+        const cardModelTitleDiv = document.createElement('div');
+        cardModelTitleDiv.classList.add('modelContainer')
+        const carBrandTitle = document.createElement('h2');
+        carBrandTitle.innerText = car.brand;
+        cardDiv.classList.add('card');
+        cardDiv.append(carBrandTitle);
+        car.models.forEach(model => {
+            console.log(model);
+            const carModelTitle = document.createElement('p');
+            carModelTitle.innerText = model;
+            cardModelTitleDiv.append(carModelTitle);
+        })
+        cardDiv.append(cardModelTitleDiv);
+        document.getElementById('output').append(cardDiv);
+    })
+
+}
+
+function getDataFromJson() {
+    fetch(ENDPOINT)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Can\'t fetch json');
+            }
+        })
+        .then(data => {
+            carBrandCard(data);
+        })
+        .catch(err => console.log(err))
+}
+getDataFromJson();
+
